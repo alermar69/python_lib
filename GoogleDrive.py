10776c4fa801d296197d1e77cc4d0ace70c03117
alermar69 <alermar69@mail.ru>
2021-03-29 15:06:38 +0300

    googledrive

---
+++ GoogleDrive.py
@@ -0,0 +1,7 @@
+class Googledrive:
+    def __init__(self):
+        self.id_file = ''
+        self.url_file = f'https://drive.google.com/uc?id={self.id_file}'
+
+    def get_file(self, name):
+        pass
